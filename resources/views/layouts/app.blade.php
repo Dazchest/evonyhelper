1da7d2036c4d9536bbf8ea56fb4ec2a7e2d8e91c
Dazchest <darrenlkitchen@gmail.com>
2022-08-20 17:34:46 +0100

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">asdasd
+<head>asdasdvdfvdfv
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
